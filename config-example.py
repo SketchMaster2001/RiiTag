df7c7e0637171a1bf09f31a66a1b263f1928281c
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-02-25 19:27:00 +0000

    Adds config file

---
+++ config-example.py
@@ -0,0 +1,9 @@
+# We use this file so Riitag can work on localhost. 
+# Useless when we get it on the VPS
+
+# Database Connection (I like postgreSQL)
+db_url = "postgresql://username:password@localhost/database_name"
+
+# Enables the admin panel if I want there to be one
+admin_panel = False
+secret_key = "please_change_this"
