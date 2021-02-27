40de499b3c8640ce33210f9a14a527cb902ff3cb
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-02-27 14:18:16 -0500

    db support (Currently Broken)

--- src/backgrounds.py
+++ src/backgrounds.py
@@ -1,16 +1,24 @@
-from main import app, db
+from main import app, db 
 from PIL import Image
 from models import UserData
 
-from flask import send_from_directory
 
-background = Image.open("assets/backgrounds/memes.jpg")
-overlay = Image.open("assets/overlays/overlay1.png")
-
-background.paste(overlay, (0, 0), overlay)
-
-background.save('assets/temp/bruh.png')
-
+def serve_background(discord_id):
+    retrieved_data = (          
+        db.session.query(UserData)
+        .filter(UserData.discord_id == discord_id)
+        .all
+    )
+    for user_data in enumerate(retrieved_data):
+        bg = user_data.background
+        overly = user_data.overlay
+    
+    
+    background = Image.open(f"assets/backgrounds/{bg}.jpg").format(bg)
+    overlay = Image.open(f"assets/overlays/{overly}.png").format(overly) 
+    
+    background.paste(overlay, (0, 0), overlay)
+    background.save('assets/temp/bck.png').format(discord_id)
 
 
 
