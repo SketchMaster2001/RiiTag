381f9002fcfce20c0a5855a7502bbfb6a05e8a4c
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-02-27 14:20:56 -0500

    Update coins.py

--- src/coins.py
+++ src/coins.py
@@ -2,10 +2,10 @@ from main import app, db
 from PIL import Image
 from models import UserData
 
-background = Image.open("assets/temp/bru.png")
+background = Image.open("assets/temp/bck.png")
 overlay = Image.open("assets/coins/animalcrossing.png")
 
 background.paste(overlay, (862, 10), overlay)
 
-background.save('assets/temp/brh.png')
+background.save('assets/temp/bckflg.png')
 
