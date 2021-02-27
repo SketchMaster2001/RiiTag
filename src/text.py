3fdc6f9dadca2c4deb261a9276b8b610ed42c20a
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-02-27 14:21:39 -0500

    Update text.py

--- src/text.py
+++ src/text.py
@@ -2,7 +2,7 @@ from main import app, db
 from PIL import Image, ImageFont, ImageDraw
 from models import UserData
 
-background = Image.open("assets/temp/brh.png")
+background = Image.open("assets/temp/coins.png")
 title_font = ImageFont.truetype('assets/fonts/humming.ttf', 50)
 
 title_text = "SketchMaster2001"
@@ -15,4 +15,4 @@ image_editable.text((95,20), title_text, (237, 230, 211), font=title_font)
 
 image_editable.text((23,385), friend_code, (237, 230, 211), font=title_font)
 
-background.save('assets/temp/hah.png')
+background.save('assets/temp/tag.png')
