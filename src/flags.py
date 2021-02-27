23536e84093f5ceb2dd5667c3464776b5e09dddc
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-02-27 14:22:02 -0500

    Update flags.py

--- src/flags.py
+++ src/flags.py
@@ -4,16 +4,16 @@ from models import UserData
 
 import os
 
-background = Image.open("assets/temp/bruh.png")
+background = Image.open("assets/temp/bck.png")
 flag = Image.open("assets/flags/ca.png")
 
 flag = flag.convert('RGBA')
 
 background.paste(flag, (12, 10), flag)
 
-background.save('assets/temp/bru.png')
+background.save('assets/temp/flag.png')
 
-os.remove('assets/temp/bruh.png')
+
 
 
 
